68076200ba3951e973807df18d0546769829a804
Jonas Verhaeghe <jonas@cultuurnet.be>
2015-09-09 11:30:13 +0200

    First part of consumerProvider

--- src/ConsumerProvider.php
+++ src/ConsumerProvider.php
@@ -10,16 +10,37 @@ namespace CultuurNet\SymfonySecurityOAuthUitid;
 
 use CultuurNet\SymfonySecurityOAuth\Model\ConsumerInterface;
 use CultuurNet\SymfonySecurityOAuth\Model\Provider\ConsumerProviderInterface;
+use CultuurNet\UitidCredentials\Command\GetConsumerCommand;
+use CultuurNet\UitidCredentials\Entities\Consumer;
+use CultuurNet\UitidCredentials\UitidCredentialsFetcher;
 
 class ConsumerProvider implements ConsumerProviderInterface
 {
 
     /**
+     * @var \CultuurNet\UitidCredentials\UitidCredentialsFetcher
+     */
+    private $fetcher;
+
+    /**
+     * ConsumerProvider constructor.
+     * @param UitidCredentialsFetcher $fetcher
+     */
+    public function __construct(UitidCredentialsFetcher $fetcher)
+    {
+        $this->fetcher = $fetcher;
+    }
+
+
+    /**
      * @param $consumerKey
-     * @return \CultuurNet\SymfonySecurityOAuth\Model\ConsumerInterface
+     * @return \CultuurNet\SymfonySecurityOAuth\Service\
      */
     public function getConsumerByKey($consumerKey)
     {
+        $uitid_consumer = $this->fetcher->getConsumer($consumerKey);
+        return $uitid_consumer;
+
         // TODO: Implement getConsumerByKey() method.
     }
 }
