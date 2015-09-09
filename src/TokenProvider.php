b20a0137e0d0ea6aaa71d762e7c2a88a9f76c274
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-09-09 11:21:27 +0200

    Add the implementation for the TokenProvider

--- src/TokenProvider.php
+++ src/TokenProvider.php
@@ -8,10 +8,26 @@
 
 namespace CultuurNet\SymfonySecurityOAuthUitid;
 
+use CultuurNet\SymfonySecurityOAuth\Model\Consumer;
 use CultuurNet\SymfonySecurityOAuth\Model\Provider\TokenProviderInterface;
+use Symfony\Component\Security\Core\User\User;
 
 class TokenProvider implements TokenProviderInterface
 {
+    /**
+     * @var \CultuurNet\UitidCredentials\UitidCredentialsFetcher
+     */
+    private $fetcher;
+
+    /**
+     * @var \CultuurNet\SymfonySecurityOAuth\Model\AccessToken
+     */
+    private $accessToken;
+
+    public function __construct($fetcher)
+    {
+        $this->fetcher = $fetcher;
+    }
 
     /**
      * @param $oauth_token
@@ -19,6 +35,30 @@ class TokenProvider implements TokenProviderInterface
      */
     public function getAccessTokenByToken($oauth_token)
     {
-        // TODO: Implement getAccessTokenByToken() method.
+        /** @var \CultuurNet\UitidCredentials\Entities\Token $uitid_token */
+        $uitid_token = $this->fetcher->getAccessToken($oauth_token);
+
+        /** @var \CultuurNet\UitidCredentials\Entities\Consumer $uitid_consumer */
+        $uitid_consumer = $uitid_token->getConsumer();
+
+        /** @var \CultuurNet\UitidCredentials\Entities\User $uitid_user */
+        $uitid_user = $uitid_token->getUser();
+
+        /** @var string $uitid_token_secret */
+        $uitid_token_secret = $uitid_token->getTokenSecret();
+
+        $consumer = new Consumer();
+        $consumer->setName($uitid_consumer->getName());
+        $consumer->setConsumerKey($uitid_consumer->getKey());
+        $consumer->setConsumerSecret($uitid_consumer->getSecret());
+
+        $user = new User($uitid_user->getUid(), $uitid_user->getNick(), $uitid_user->getEmail());
+
+        $this->accessToken->setConsumer($consumer);
+        $this->accessToken->setToken($uitid_token->getToken());
+        $this->accessToken->setUser($user);
+        $this->accessToken->setSecret($uitid_token_secret);
+
+        return $this->accessToken;
     }
 }
